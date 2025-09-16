7303d3637ec76146372bc8bcfca5f2f42528c00b
Асеев Станислав <Vazelin117@gmail.com>
2025-09-16 17:34:32 +0300

    Add test on start page

--- Test/start_page_tests.py
+++ Test/start_page_tests.py
@@ -1,9 +1,7 @@
-import time
-
 from Data.start_page_data import StartPageData
 from Locators.strat_page_locators import StartPageLocators
 from Pages.start_page import StartPage
-from demoqa.conftests import driver
+from conftests import driver
 
 
 class TestStartPage:
@@ -30,3 +28,13 @@ class TestStartPage:
         start_page.click(start_page_locators.get_locator(start_page_data.ELEMENTS))
 
         assert start_page_locators.get_locator_after_transit(start_page_data.ELEMENTS)
+
+    def test_after_forms(self, driver):
+        start_page = StartPage(driver)
+        start_page_locators = StartPageLocators()
+        start_page_data = StartPageData()
+
+        start_page.open()
+        start_page.click(start_page_locators.get_locator(start_page_data.FORMS))
+
+        assert start_page_locators.get_locator_after_transit(start_page_data.FORMS)
